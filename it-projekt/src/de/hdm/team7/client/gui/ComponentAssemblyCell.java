9623a1bcc2ff8c7fa4057e2b1694a60f11c8e684
MariusKlepser <marius.klepser@googlemail.com>
2015-06-18 10:45:23 +0200

    Cells hinzugefügt.

---
+++ it-projekt/src/de/hdm/team7/client/gui/ComponentAssemblyCell.java
@@ -0,0 +1,32 @@
+package de.hdm.team7.client.gui;
+import com.google.gwt.cell.client.AbstractCell;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+
+import de.hdm.team7.shared.businessObjects.*;
+
+
+
+/**
+ * Klasse zur Darstellung von Customer-Objekten.
+ * Solche Erweiterungen von <code>AbstractCell<T></code> dienen zur Erzeugung von
+ * HTML-Code fÃ¼r benutzerdefinierte Objekte. In diesem Fall werden die Werte der Attribute
+ * <code>lastName</code> und <code>firstName</code> eines Kundenobjekts mit einem Komma
+ * und einer Leerstelle in einem <code>div-</code>Element
+ * erzeugt.
+ * 
+ * In Anlehnung an: @author rathke
+ *
+ */
+public class ComponentAssemblyCell extends AbstractCell<ComponentAssembly> {
+	@Override
+    public void render(Context context, ComponentAssembly value, SafeHtmlBuilder sb) {
+      // Value can be null, so do a null check..
+      if (value == null) {
+        return;
+      }
+
+      sb.appendHtmlConstant("<div>");
+      sb.appendEscaped(value.getName());
+      sb.appendHtmlConstant("</div>");
+    }
+}
