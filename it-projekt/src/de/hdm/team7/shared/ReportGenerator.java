91933b3d54e624e1615319ca346d66b09aca9ffd
Dominik Dach <dd032@hdm-stuttgart.de>
2015-06-22 20:07:51 +0200

    Neue Gruppe und Klassen erstellt für den ReportGenerator
    ToDo: Restliche Klassen im .shared erstellen

--- it-projekt/src/de/hdm/team7/shared/ReportGenerator.java
+++ it-projekt/src/de/hdm/team7/shared/ReportGenerator.java
@@ -1,5 +1,7 @@
 package de.hdm.team7.shared;
 
+import de.hdm.team7.shared.geschäftsobjekte.*;
+
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
@@ -14,5 +16,34 @@ public interface ReportGenerator extends RemoteService {
 	   * 
 	   * @throws IllegalArgumentException
 	   */
-	  public void init() throws IllegalArgumentException;	
-}
+	  public void init() throws IllegalArgumentException;
+	  
+	  /**
+	   * Setzen der zugeordneten Stückliste?.
+	   * 
+	   * @para Stücklisten-Objekt? 
+	   * @throws IllegalArgumentException
+	   * 
+	   * NOTWENDIG?
+	   */
+	  public void setStückliste(Stückliste s) throws IllegalArgumentException;
+
+	  /**
+	   * Erstellen eines <code>AllAccountsOfCustomerReport</code>-Reports. Dieser
+	   * Report-Typ stellt sÃƒÂ¤mtliche Konten eines Kunden dar.
+	   * 
+	   * @param s eine Referenz auf die Stückliste bzgl. dessen der Report
+	   *          erstellt werden soll.
+	   * @return das fertige Reportobjekt
+	   * @throws IllegalArgumentException
+	   * @see StücklisteAnzeigenReport
+	   */
+	  public abstract StücklisteAnzeigenReport erstelleStücklistenReport(
+	      Stückliste s) throws IllegalArgumentException;
+
+	  // Weitere Methoden hier eintragen die notwendig sind
+	  
+	  
+	  
+	  
+	}
