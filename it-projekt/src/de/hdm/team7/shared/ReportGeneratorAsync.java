eb3eb86c1dec843ae2dfa709788dda87482238a3
MariusKlepser <marius.klepser@googlemail.com>
2015-06-23 12:02:37 +0200

    Test 2

--- it-projekt/src/de/hdm/team7/shared/ReportGeneratorAsync.java
+++
@@ -1,9 +0,0 @@
-package de.hdm.team7.shared;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface ReportGeneratorAsync {
-	
-	void init(AsyncCallback<Void> callback);
-
-}
--- it-projekt/src/de/hdm/team7/shared/geschaeftsobjekte/Enderzeugnis.java
+++
@@ -1,7 +0,0 @@
-package de.hdm.team7.shared.geschaeftsobjekte;
-
-public class Enderzeugnis extends Baugruppe {
-
-	private static final long serialVersionUID = 1L;
-
-}
