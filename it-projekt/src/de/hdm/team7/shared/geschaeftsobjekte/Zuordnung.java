c82e931a21825080bf022aecad222c64964e095b
Philipp Schmitt Vierheilig <ps091@hdm-stuttgart.de>
2015-06-25 12:49:03 +0200

    Erstellung des Zuordnungsmappers sowie einer Superklasse Zuordnung;
    TODO: Mapping für sämtliche möglichen Kombinationen implementieren;
    Subklassen für sämtliche möglichen Kombinationen implementieren;

---
+++ it-projekt/src/de/hdm/team7/shared/geschaeftsobjekte/Zuordnung.java
@@ -0,0 +1,60 @@
+package de.hdm.team7.shared.geschaeftsobjekte;
+
+import java.util.Date;
+
+public class Zuordnung {
+
+	private int id;
+	private int menge;
+	private Date aenderungsdatum;
+	private Benutzer aenderer;
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param materialBezeichnung the materialIdentifier to set
+	 */
+	public int getMenge() {
+		return menge;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public Date getAenderungsdatum() {
+		return aenderungsdatum;
+	}
+
+	/**
+	 * @param beschreibung the description to set
+	 */
+	public Benutzer getAenderer() {
+		return aenderer;
+	}
+
+	/**
+	 * @param aenderungsDatum the aenderungsDatum to set
+	 */
+	public void setMenge(int menge) {
+		this.menge = menge;
+	}
+	
+	/**
+	 * @param aenderungsDatum the aenderungsDatum to set
+	 */
+	public void setAenderungsdatum(Date aenderungsdatum) {
+		this.aenderungsdatum = aenderungsdatum;
+	}
+	
+	public void setAenderer(Benutzer aenderer) {
+		this.aenderer = aenderer;
+	}
+	
+	public String toString(){
+		return this.getId()+", "+this.getMenge()+", "+this.getAenderungsdatum()+", "+this.getAenderer();
+	}
+	
+}
