987f1cb77f8c5dca6e8627dce004522d23cf7a1d
KevinJaeger <kj015@hdm-stuttgart.de>
2015-07-01 14:29:22 +0200

    -Löschungen: Session/User

--- it-projekt/src/de/hdm/team7/client/SessionService.java
+++
@@ -1,9 +0,0 @@
-package de.hdm.team7.client;
-
-import com.google.gwt.user.client.rpc.RemoteService;
-import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-
-@RemoteServiceRelativePath("session")
-public interface SessionService extends RemoteService {
-	SessionId session(SessionId sessionId);
-}// end class
--- it-projekt/src/de/hdm/team7/client/SessionServiceAsync.java
+++
@@ -1,7 +0,0 @@
-package de.hdm.team7.client;
-
-import com.google.gwt.user.client.rpc.AsyncCallback;
-
-public interface SessionServiceAsync {
-	void session(SessionId sessionId, AsyncCallback<SessionId> asyncCallback);
-}// end MyServiceAsync
