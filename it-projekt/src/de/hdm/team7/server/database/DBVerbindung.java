a20a307026880d38e0e6449f9d42c280184176f8
Philipp Schmitt Vierheilig <ps091@hdm-stuttgart.de>
2015-07-06 14:36:58 +0200

    No Cloud Sql

--- it-projekt/src/de/hdm/team7/server/database/DBVerbindung.java
+++ it-projekt/src/de/hdm/team7/server/database/DBVerbindung.java
@@ -56,7 +56,7 @@ public class DBVerbindung {
         		
         			    // Load the class that provides the new "jdbc:google:mysql://" prefix.
         			    Class.forName("com.mysql.jdbc.GoogleDriver");
-        			    url = "jdbc:google:mysql://it-projekt:it-projekt/whatsgoes?user=root";
+        			    url = "jdbc:msql://213.165.82.134:3306/whatsgoes";
         			  
 
                 /*
@@ -67,7 +67,7 @@ public class DBVerbindung {
                  * Diese Verbindung wird dann in der statischen Variable con
                  * abgespeichert und fortan verwendet.
                  */
-                connection = DriverManager.getConnection(url);
+                connection = DriverManager.getConnection(url, user, password);
                 
             }
         	catch (Exception e) {
